be562e112297edc34dd7de0d5024f09d2974a3eb
Paul D'Ambra <paul.damba@coop.co.uk>
2020-11-28 11:19:16 +0000

    test with make locally

---
+++ makefile
@@ -0,0 +1,9 @@
+
+dependencies:
+	PIPENV_VENV_IN_PROJECT=1 pipenv install --dev
+
+lint: dependencies
+	pipenv run flake8
+
+test: lint
+	pipenv run nosetests
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Advent-of-code-2020',
     version='0..0.0',
-    packages=['dambra.adventofcode',],
+    packages=['dambra.adventofcode', ],
     license='The Unlicense',
     long_description=open('README.md').read(),
 )
