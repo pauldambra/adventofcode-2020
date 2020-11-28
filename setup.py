5ba154227a16d712786dc3caf38c733ed9ed9167
Paul D'Ambra <paul.damba@coop.co.uk>
2020-11-28 10:28:35 +0000

    get a test runner setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Advent-of-code-2020',
+    version='0..0.0',
+    packages=['dambra.adventofcode',],
+    license='The Unlicense',
+    long_description=open('README.md').read(),
+)
