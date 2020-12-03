e8dadcda4326426b4635d21235c78baeaa62f8b3
Paul D'Ambra <paul.damba@coop.co.uk>
2020-12-03 20:23:47 +0000

    can be off the bottom of the map - day three

--- day_three/day_three_tests.py
+++ day_three/day_three_tests.py
@@ -1,9 +1,21 @@
 import unittest
+from dataclasses import dataclass
+
+
+@dataclass
+class CoordinateLookup:
+    within_map: bool
+    x: int
+    y: int
+    encountered: str
 
 
 def read(x, y, map_rows):
-    wrapped_index = x % len(map_rows[0])
-    return map_rows[y][wrapped_index]
+    if y >= len(map_rows):
+        return CoordinateLookup(False, x, y, None)
+    else:
+        wrapped_index = x % len(map_rows[0])
+        return CoordinateLookup(True, x, y, map_rows[y][wrapped_index])
 
 
 class DayThreeTests(unittest.TestCase):
@@ -13,26 +25,37 @@ class DayThreeTests(unittest.TestCase):
         one = read(0, 0, map)
         two = read(1, 0, map)
 
-        self.assertEqual(one, '#')
-        self.assertEqual(two, '.')
+        self.assertEqual(one.encountered, '#')
+        self.assertEqual(two.encountered, '.')
 
     def test_get_coordinates_after_map_row_wrapped(self):
         map = "#.#.#...........#.....#.#....##".split("\n")
         one = read(len(map[0]), 0, map)
 
-        self.assertEqual(one, '#')
+        self.assertEqual(one.encountered, '#')
 
     def test_get_coordinates_from_row_one(self):
         map = "#.#.#.\n.#.#.#".split("\n")
         one = read(0, 1, map)
 
-        self.assertEqual(one, '.')
+        self.assertEqual(one.encountered, '.')
 
     def test_get_wrapped_coordinates_from_row_one(self):
         map = "#.#.#.\n.#.#.#".split("\n")
         one = read(7, 1, map)
 
-        self.assertEqual(one, '#')
+        self.assertEqual(one.encountered, '#')
+
+    def test_never_outside_of_map_for_x_row(self):
+        map = "#.#.#.\n.#.#.#".split("\n")
+        results = [read(i, 0, map) for i in range(0, 100)]
+        for x in results:
+            self.assertTrue(x.within_map)
+
+    def test_can_be_outside_of_map_for_y_row(self):
+        map = "#.#.#.\n.#.#.#".split("\n")
+        result = read(0, len(map), map)
+        self.assertFalse(result.within_map)
 
 
 if __name__ == '__main__':
