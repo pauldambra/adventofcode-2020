ae4fb8c3401c7d7b222684dda4f5561d11ec73e1
Paul D'Ambra <paul.damba@coop.co.uk>
2020-12-06 14:52:48 +0000

    day 6 part 2 solution

--- day_six/day_six_tests.py
+++ day_six/day_six_tests.py
@@ -35,6 +35,31 @@ def any_yes(groups):
     return [sorted(list(set(g))) for g in prepared_answers]
 
 
+def keep_shared(peoples_answers):
+    num_of_people = len(peoples_answers)
+    seen_answers = {}
+    for answers in peoples_answers:
+        for answer in answers:
+            if answer not in seen_answers:
+                seen_answers[answer] = 0
+
+            seen_answers[answer] = seen_answers[answer] + 1
+
+    everyone_answered = [
+        k for (k, _) in seen_answers.items()
+        if seen_answers[k] == num_of_people
+    ]
+
+    return everyone_answered
+
+
+def everyone_yes(groups):
+    unique_per_person = [[sorted(list(set(x))) for x in groups_answers]
+                         for groups_answers in groups]
+    everyone_said_yes = [keep_shared(answers) for answers in unique_per_person]
+    return everyone_said_yes
+
+
 def sum_counts(groups):
     return sum([len(x) for x in groups])
 
@@ -83,4 +108,14 @@ class DaySixTests(unittest.TestCase):
         with open(get_puzzle_input_path(os.path.dirname(__file__))) as content:
             ss = content.read()
         sum_of_counts = sum_counts(any_yes(split_groups(ss)))
+        self.assertEqual(sum_of_counts, 6686)
+
+    def test_sum_part_two_counts_for_example_input(self):
+        sum_of_counts = sum_counts(everyone_yes(split_groups(example_input)))
+        self.assertEqual(sum_of_counts, 6)
+
+    def test_sum_counts_for_puzzle_input_part_two(self):
+        with open(get_puzzle_input_path(os.path.dirname(__file__))) as content:
+            ss = content.read()
+        sum_of_counts = sum_counts(everyone_yes(split_groups(ss)))
         self.assertEqual(sum_of_counts, 6686)
