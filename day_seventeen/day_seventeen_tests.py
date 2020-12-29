c4c66c4c201b7aad2f1526aff352a2c09a854ddf
Paul D'Ambra <paul.damba@coop.co.uk>
2020-12-29 22:41:46 +0000

    day 17 part 2 solution

--- day_seventeen/day_seventeen_tests.py
+++ day_seventeen/day_seventeen_tests.py
@@ -22,14 +22,10 @@ puzzle_input = """
 ######.#
 """
 
-
 @dataclass(frozen=True)
 class Coordinate:
-    x: int
-    y: int
-    z: int
 
-    __neighbour_calculations = [
+    neighbour_calculations = [
         lambda coord: replace(coord, x=coord.x - 1),
         lambda coord: replace(coord, x=coord.x - 1, y=coord.y - 1),
         lambda coord: replace(coord, y=coord.y - 1),
@@ -41,7 +37,56 @@ class Coordinate:
     ]
 
     def neighbours(self) -> list[Coordinate]:
-        this_plane = [x(self) for x in self.__neighbour_calculations]
+        raise NotImplementedError("Must implemented in a subclass")
+
+
+@dataclass(frozen=True)
+class FourDimensionalCoordinate(Coordinate):
+    x: int
+    y: int
+    z: int
+    w: int
+
+    def neighbours(self) -> list[ThreeDimensionalCoordinate]:
+        this_plane = [x(self) for x in self.neighbour_calculations]
+        this_plane_w1 = [replace(coord, w=coord.w - 1) for coord in this_plane]
+        this_plane_w2 = [replace(coord, w=coord.w + 1) for coord in this_plane]
+        second_plane = [replace(coord, z=coord.z - 1) for coord in this_plane]
+        second_plane_w1 = [replace(coord, w=coord.w - 1) for coord in second_plane]
+        second_plane_w2 = [replace(coord, w=coord.w + 1) for coord in second_plane]
+        third_plane = [replace(coord, z=coord.z + 1) for coord in this_plane]
+        third_plane_w1 = [replace(coord, w=coord.w - 1) for coord in third_plane]
+        third_plane_w2 = [replace(coord, w=coord.w + 1) for coord in third_plane]
+
+        results = []
+        results.extend(this_plane)
+        results.extend(this_plane_w1)
+        results.extend(this_plane_w2)
+        results.extend(second_plane)
+        results.extend(second_plane_w1)
+        results.extend(second_plane_w2)
+        results.extend(third_plane)
+        results.extend(third_plane_w1)
+        results.extend(third_plane_w2)
+        results.append(replace(self, w=self.w - 1))
+        results.append(replace(self, w=self.w + 1))
+        results.append(replace(self, z=self.z - 1))
+        results.append(replace(self, z=self.z - 1, w=self.w - 1))
+        results.append(replace(self, z=self.z - 1, w=self.w + 1))
+        results.append(replace(self, z=self.z + 1))
+        results.append(replace(self, z=self.z + 1, w=self.w - 1))
+        results.append(replace(self, z=self.z + 1, w=self.w + 1))
+        return results
+
+
+@dataclass(frozen=True)
+class ThreeDimensionalCoordinate(Coordinate):
+    x: int
+    y: int
+    z: int
+
+    def neighbours(self) -> list[ThreeDimensionalCoordinate]:
+        this_plane = [x(self) for x in self.neighbour_calculations]
         second_plane = [replace(coord, z=coord.z - 1) for coord in this_plane]
         third_plane = [replace(coord, z=coord.z + 1) for coord in this_plane]
 
@@ -56,9 +101,9 @@ class Coordinate:
 
 @dataclass
 class PocketDimension:
-    coordinates: dict[Coordinate, Literal['.', '#']]
+    coordinates: dict[ThreeDimensionalCoordinate, Literal['.', '#']]
 
-    def get(self, coord: Coordinate) -> Literal['.', '#']:
+    def get(self, coord: ThreeDimensionalCoordinate) -> Literal['.', '#']:
         return self.__get(self.coordinates, coord)
 
     @staticmethod
@@ -99,10 +144,10 @@ class PocketDimension:
             in starting_state.splitlines()
             if len(x.strip()) > 0
         ]
-        results: dict[Coordinate, Literal['.', '#']] = {}
+        results: dict[ThreeDimensionalCoordinate, Literal['.', '#']] = {}
         for y, row in enumerate(rows):
             for x, col in enumerate(row):
-                results[Coordinate(x, y, 0)] = col
+                results[ThreeDimensionalCoordinate(x, y, 0)] = col
 
         return PocketDimension(results)
 
@@ -122,12 +167,26 @@ class PocketDimension:
         print(plane_extent)
         for y in range(min_y, max_y + 1):
             for x in range(min_x, max_x + 1):
-                drawing += self.__get(self.coordinates, Coordinate(x, y, z))
+                drawing += self.__get(self.coordinates, ThreeDimensionalCoordinate(x, y, z))
             drawing += "\n"
         return drawing
 
     def count_active(self):
         return sum(1 for x in self.coordinates.values() if x == '#')
+
+    @classmethod
+    def parse_4d(cls, starting_state: str) -> PocketDimension:
+        rows = [
+            x.strip() for x
+            in starting_state.splitlines()
+            if len(x.strip()) > 0
+        ]
+        results: dict[FourDimensionalCoordinate, Literal['.', '#']] = {}
+        for y, row in enumerate(rows):
+            for x, col in enumerate(row):
+                results[FourDimensionalCoordinate(x, y, 0, 0)] = col
+
+        return PocketDimension(results)
 
 
 class DaySeventeenTests(unittest.TestCase):
@@ -144,15 +203,15 @@ class DaySeventeenTests(unittest.TestCase):
          1 b a f
          2 i h g
         """
-        a = Coordinate(0, 1, 2)
-        b = Coordinate(-1, 1, 2)
-        c = Coordinate(-1, 0, 2)
-        d = Coordinate(0, 0, 2)
-        e = Coordinate(1, 0, 2)
-        f = Coordinate(1, 1, 2)
-        g = Coordinate(1, 2, 2)
-        h = Coordinate(0, 2, 2)
-        i = Coordinate(-1, 2, 2)
+        a = ThreeDimensionalCoordinate(0, 1, 2)
+        b = ThreeDimensionalCoordinate(-1, 1, 2)
+        c = ThreeDimensionalCoordinate(-1, 0, 2)
+        d = ThreeDimensionalCoordinate(0, 0, 2)
+        e = ThreeDimensionalCoordinate(1, 0, 2)
+        f = ThreeDimensionalCoordinate(1, 1, 2)
+        g = ThreeDimensionalCoordinate(1, 2, 2)
+        h = ThreeDimensionalCoordinate(0, 2, 2)
+        i = ThreeDimensionalCoordinate(-1, 2, 2)
         first_plane = [a, b, c, d, e, f, g, h, i]
         second_plane = [replace(coord, z=1) for coord in first_plane]
         third_plane = [replace(coord, z=3) for coord in first_plane]
@@ -175,7 +234,7 @@ class DaySeventeenTests(unittest.TestCase):
         z = randint(0, 999999999)
         self.assertEqual(
             '.',
-            pd.get(Coordinate(x, y, z))
+            pd.get(ThreeDimensionalCoordinate(x, y, z))
         )
 
     def test_pocket_dimension_can_start_with_non_dot_coords(self):
@@ -184,19 +243,19 @@ class DaySeventeenTests(unittest.TestCase):
         z = randint(0, 999999999)
 
         pd = PocketDimension({
-            Coordinate(x, y, z): '#'
+            ThreeDimensionalCoordinate(x, y, z): '#'
         })
 
         self.assertEqual(
             '#',
-            pd.get(Coordinate(x, y, z))
+            pd.get(ThreeDimensionalCoordinate(x, y, z))
         )
 
     def test_can_parse_string_input_to_starting_coordinates(self):
         pd = PocketDimension.parse(example_input)
         self.assertEqual(
             '#',
-            pd.get(Coordinate(1, 0, 0))
+            pd.get(ThreeDimensionalCoordinate(1, 0, 0))
         )
 
     def test_can_tick(self):
@@ -219,32 +278,60 @@ class DaySeventeenTests(unittest.TestCase):
         """
         pd = PocketDimension.parse(example_input)
         pd.tick()
-        self.assertEqual('#', pd.get(Coordinate(0, 1, -1)))
+        self.assertEqual('#', pd.get(ThreeDimensionalCoordinate(0, 1, -1)))
         self.assertEqual(11, pd.count_active())
 
     def test_can_tick_thrice_from_example(self):
         pd = PocketDimension.parse(example_input)
-        pd.tick()
-        pd.tick()
-        pd.tick()
+        for _ in range(0, 3):
+            pd.tick()
+
         self.assertEqual(38, pd.count_active())
 
     def test_can_tick_six_from_example(self):
         pd = PocketDimension.parse(example_input)
-        pd.tick()
-        pd.tick()
-        pd.tick()
-        pd.tick()
-        pd.tick()
-        pd.tick()
+        for _ in range(0, 6):
+            pd.tick()
+
         self.assertEqual(112, pd.count_active())
 
     def test_can_tick_six_for_part_one(self):
         pd = PocketDimension.parse(puzzle_input)
-        pd.tick()
-        pd.tick()
-        pd.tick()
-        pd.tick()
-        pd.tick()
-        pd.tick()
+        for _ in range(0, 6):
+            pd.tick()
+
         self.assertEqual(388, pd.count_active())
+
+    def test_can_get_neighbours_in_4d(self):
+        c = FourDimensionalCoordinate(0, 0, 0, 0)
+        self.assertEqual(
+            80,
+            len(c.neighbours())
+        )
+
+    def test_can_parse_in_4d(self):
+        pd = PocketDimension.parse_4d(example_input)
+        self.assertEqual(
+            '#',
+            pd.get(FourDimensionalCoordinate(1, 0, 0, 0))
+        )
+
+    def test_six_ticks_for_part_two_example(self):
+        pd = PocketDimension.parse_4d(example_input)
+        for _ in range(0, 6):
+            pd.tick()
+
+        self.assertEqual(
+            848,
+            pd.count_active()
+        )
+
+    def test_six_ticks_for_part_two(self):
+        pd = PocketDimension.parse_4d(puzzle_input)
+        for _ in range(0, 6):
+            pd.tick()
+
+        self.assertEqual(
+            2280,
+            pd.count_active()
+        )
